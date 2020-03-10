f86df80e30092bb516be71d7eb26baf1cdeb3b1d
lionneclement <52818512+lionneclement@users.noreply.github.com>
2020-03-10 14:10:16 +0100

    add phpcs in index.php and twigenvi.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,15 @@
 <?php
-
+/** 
+ * The file is the index of this repository
+ * 
+ * PHP version 7.2.18
+ * 
+ * @category Public
+ * @package  Public
+ * @author   Clement <lionneclement@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 require '../../vendor/autoload.php';
 require '../../src/twig/twigenvi.php';
 require '../controller/postController.php';
@@ -9,96 +19,134 @@ require '../model/adminmodel.php';
 require '../model/postmodel.php';
 require '../model/entity.php';
 
-use App\controller\admincontroller;
-use App\controller\postcontroller;
+use App\controller\Admincontroller;
+use App\controller\Postcontroller;
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable('../../');
 $dotenv->load();
 $router = new AltoRouter();
 
- $router->map('GET|POST', '/', function() {
-	$page = new postcontroller;
-    $page->home($_POST);
-});
- $router->map('GET', '/posts', function() {
-    $page = new postcontroller;
-    $page->allposts();
-});
-$router->map('GET', '/post/[i:id]', function($id) {
-    $page = new postcontroller;
-    $page->onepost($id);
-});
-$router->map('GET|POST', '/admin/addpost', function() {
-    $page = new postcontroller;
-    $page->add($_POST);
-});
-$router->map('GET|POST', '/updatepost/[i:id]', function($id) {
-    $page = new postcontroller;
-    $page->update($id,$_POST); 
-});
-$router->map('GET', '/removepost/[i:id]', function($id) {
-    $page = new postcontroller;
-    $page->remove($id);
-});
-$router->map('GET|POST','/register',function() {
-    $page = new admincontroller;
-    $page->register($_POST);
-});
-$router->map('GET|POST','/login',function() {
-    $page = new admincontroller;
-    $page->login($_POST);
-});
-$router->map('GET','/logout',function(){
-    $page = new admincontroller;
-    $page->logout();
-});
-$router->map('GET|POST','/admin/roles',function(){
-    $page = new admincontroller;
-    $page->roles($_POST);
-});
-$router->map('GET','/admin',function(){
-    $page = new admincontroller;
-    $page->admin();
-});
-$router->map('POST','/comment',function(){
-    $page = new postcontroller;
-    $page->comment($_POST);
-});
-$router->map('GET|POST','/admin/[a:action]',function($action){
-    $page = new admincontroller;
-    $page->comment($_POST,$action);
-});
-$router->map('GET','/admin/deletecomment/[i:id]/[a:action]',function($id,$action){
-    $page = new admincontroller;
-    $page->deletecomment($id,$action);
-});
-$router->map('GET','/admin/deleteuser/[i:id]',function($id){
-    $page = new admincontroller;
-    $page->deleteuser($id);
-});
-$router->map('GET|POST','/resetpassword',function(){
-    $page = new admincontroller;
-    $page->resetpassword($_POST);
-});
-$router->map('GET|POST','/resetlink/[i:id]/[a:action]',function($id,$action){
-    $page = new admincontroller;
-    $page->resetlink($id,$action,$_POST);
-});
-$router->map('GET|POST','/updateuser',function(){
-    $page = new admincontroller;
-    $page->updateuser($_POST);
-});
-$router->map('GET|POST','/updatepassword',function(){
-    $page = new admincontroller;
-    $page->updatepassword($_POST);
-});
+$router->map(
+    'GET|POST', '/', function () {
+        $page = new postcontroller;
+        $page->home($_POST);
+    }
+);
+$router->map(
+    'GET', '/posts', function () {
+        $page = new postcontroller;
+        $page->allposts();
+    }
+);
+$router->map(
+    'GET', '/post/[i:id]', function ($id) {
+        $page = new postcontroller;
+        $page->onepost($id);
+    }
+);
+$router->map(
+    'GET|POST', '/admin/addpost', function () {
+        $page = new postcontroller;
+        $page->add($_POST);
+    }
+);
+$router->map(
+    'GET|POST', '/updatepost/[i:id]', function ($id) {
+        $page = new postcontroller;
+        $page->update($id, $_POST); 
+    }
+);
+$router->map(
+    'GET', '/removepost/[i:id]', function ($id) {
+        $page = new postcontroller;
+        $page->remove($id);
+    }
+);
+$router->map(
+    'GET|POST', '/register', function () {
+        $page = new admincontroller;
+        $page->register($_POST);
+    }
+);
+$router->map(
+    'GET|POST', '/login', function () {
+        $page = new admincontroller;
+        $page->login($_POST);
+    }
+);
+$router->map(
+    'GET', '/logout', function () {
+        $page = new admincontroller;
+        $page->logout();
+    }
+);
+$router->map(
+    'GET|POST', '/admin/roles', function () {
+        $page = new admincontroller;
+        $page->roles($_POST);
+    }
+);
+$router->map(
+    'GET', '/admin', function () {
+        $page = new admincontroller;
+        $page->admin();
+    }
+);
+$router->map(
+    'POST', '/comment', function () {
+        $page = new postcontroller;
+        $page->comment($_POST);
+    }
+);
+$router->map(
+    'GET|POST', '/admin/[a:action]', function ($action) {
+        $page = new admincontroller;
+        $page->comment($_POST, $action);
+    }
+);
+$router->map(
+    'GET', '/admin/deletecomment/[i:id]/[a:action]', function ($id,$action) {
+        $page = new admincontroller;
+        $page->deletecomment($id, $action);
+    }
+);
+$router->map(
+    'GET', '/admin/deleteuser/[i:id]', function ($id) {
+        $page = new admincontroller;
+        $page->deleteuser($id);
+    }
+);
+$router->map(
+    'GET|POST', '/resetpassword', function () {
+        $page = new admincontroller;
+        $page->resetpassword($_POST);
+    }
+);
+$router->map(
+    'GET|POST', '/resetlink/[i:id]/[a:action]', function ($id,$action) {
+        $page = new admincontroller;
+        $page->resetlink($id, $action, $_POST);
+    }
+);
+$router->map(
+    'GET|POST', '/updateuser', function () {
+        $page = new admincontroller;
+        $page->updateuser($_POST);
+    }
+);
+$router->map(
+    'GET|POST', '/updatepassword', function () {
+        $page = new admincontroller;
+        $page->updatepassword($_POST);
+    }
+);
 
   
-$match = $router->match();
+ $match = $router->match();
 
-if( is_array($match) && is_callable( $match['target'] ) ) {
-	call_user_func_array( $match['target'], $match['params'] ); 
+if (is_array($match) && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']); 
 } else {
-	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
 }
--- src/twig/twigenvi.php
+++ src/twig/twigenvi.php
@@ -1,14 +1,38 @@
 <?php
+/** 
+ * The file it's for init twig
+ * 
+ * PHP version 7.2.18
+ * 
+ * @category Twig
+ * @package  Twig
+ * @author   Clement <lionneclement@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 namespace App\twig;
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
-
-class twigenvi
+/** 
+ * This it's for init twig
+ * 
+ * PHP version 7.2.18
+ * 
+ * @category Twig
+ * @package  Twig
+ * @author   Clement <lionneclement@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+class Twigenvi
 {
-  public function __construct()
-  {
-    $loader = new FilesystemLoader('../../src/view');
-    $this->twigenvi = new Environment($loader);
-  }
+    /**
+     * Init twig
+     */
+    public function __construct()
+    {
+        $loader = new FilesystemLoader('../../src/view');
+        $this->twigenvi = new Environment($loader);
+    }
 }
